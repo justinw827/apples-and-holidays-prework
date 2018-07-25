edbb9eefaac3d6d393bc277eb71b781a723eef0a
Justin Wang <justinw827@aol.com>
2018-07-25 18:01:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |key, value|
-    puts key.capitalize + ':'
+    case season
+    when ":winter"
+      puts "Winter:"
 end
 
 def all_holidays_with_bbq(holiday_hash)
