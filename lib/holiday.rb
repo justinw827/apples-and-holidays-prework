b0655be2313c4e63150d1ef72c8469c6f7eb550b
Justin Wang <justinw827@aol.com>
2018-07-25 18:54:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  myArray = []
   holiday_hash.each do |key, value|
-
+    value.each do |key2, value2|
+      if value2.find_index then myArray.push(key2) end
+    end
   end
 end
